678af29c189403cedc1f7da6056c50e082edfcf6
daniecon <daniel.contreras@math.ethz.ch>
2022-04-10 21:40:13 +0200

    Implemented Vue Router

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,12 @@
 import './main.css'
 
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
 import { createApp } from 'vue'
 import App from './App.vue'
 

9e4fcabf3b71f88561a407dd27afde91e2cea0f2
daniecon <daniel.contreras@math.ethz.ch>
2022-04-12 00:53:15 +0200

    Updated send-tweet with program data

--- app/src/api/send-tweet.js
+++ app/src/api/send-tweet.js
@@ -1,10 +1,27 @@
+import { web3 } from '@project-serum/anchor'
+import { useWorkspace } from '@/composables'
+import { Tweet } from '@/models'
+
+
 export const sendTweet = async (topic, content) => {
-    return {
-        topic,
-        content,
-        author_display: 'B1Af..wtRN',
-        created_at: 'Nov 26, 2021 1:03PM',
-        created_ago: 'just now',
-        timestamp: 1637932868,
-    }
+    const { wallet, program } = useWorkspace()
+
+    // New Keypair for our new tweet account.
+    const tweet = web3.Keypair.generate()
+
+    // Send a "SendTweet" instruction
+    await program.value.rpc.sendTweet(topic, content, {
+        accounts: {
+            author: wallet.value.publicKey,
+            tweet: tweet.publicKey,
+            systemProgram: web3.SystemProgram.programId,
+        },
+        signers: [tweet]
+    })
+
+    // Fetch the newly created account from the blockchain.
+    const tweetAccount = await program.value.account.tweet.fetch(tweet.publicKey)
+
+    // Wrap the fetched account in a Tweet model so our frontend can display it.
+    return new Tweet(tweet.publicKey, tweetAccount)
 }

8be616baf17bd36ae2e15c50f66e0c2d0cf99053
daniecon <daniel.contreras@math.ethz.ch>
2022-04-12 11:04:20 +0200

    Deployed to devnet

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,7 +2,7 @@ import { computed } from 'vue'
 import { Program, Provider } from '@project-serum/anchor'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { useAnchorWallet } from 'solana-wallets-vue'
-import idl from '../../../target/idl/solana_twitter.json'
+import idl from '@/idl/solana_twitter.json'
 
 const preflightCommitment = 'processed'
 const commitment = 'processed'
@@ -13,7 +13,7 @@ export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const connection = new Connection('https://api.devnet.solana.com', commitment)
     const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
